4f5684f7cf3b6f257656c224e010e8fc997aea4b
Yeison Rodriguez <yrrodriguezb@gmail.com>
2022-02-03 12:01:48 -0500

    Implemented interfaces and classes for Domain.Core

---
+++ Domain/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+        void Suscribe<T, TH>() where T : Event where TH : IEventHandler<T>;
+    }
+}
---
+++ Domain/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
