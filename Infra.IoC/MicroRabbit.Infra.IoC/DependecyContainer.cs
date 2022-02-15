3b64f4de6b12511a2a73610b5e0ee52e28f9f202
Yeison Rodriguez <yrrodriguezb@gmail.com>
2022-02-14 21:57:23 -0500

    Initial commit of infrastructure Dependency Project

---
+++ Infra.IoC/MicroRabbit.Infra.IoC/DependecyContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependecyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
